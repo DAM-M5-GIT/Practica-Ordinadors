eb18f000039745c24865fce2dfea1303899d3e11
Jonathan Fullola <jonatan.f.siscart@gmail.com>
2017-04-06 15:55:28 +0200

    Jonathan i Escri (Aljo Team)
    Menú númeric, Definició d'atributs i metodes (operacions) amb els seus
    paràmetres

---
+++ clasesGithub/src/dos/Ordinador.java
@@ -0,0 +1,54 @@
+package dos;
+
+import java.io.Serializable;
+
+public class Ordinador implements Serializable{
+	
+	private String model="";
+	private String size="";
+	private String cpu="";
+	private String ram="";
+	
+	
+	public String getModel() {
+		return model;
+	}
+	public void setModel(String model) {
+		this.model = model;
+	}
+	public String getSize() {
+		return size;
+	}
+	public void setSize(String size) {
+		this.size = size;
+	}
+	public String getCpu() {
+		return cpu;
+	}
+	public void setCpu(String cpu) {
+		this.cpu = cpu;
+	}
+	public String getRam() {
+		return ram;
+	}
+	public void setRam(String ram) {
+		this.ram = ram;
+	}
+	
+	public String toString() {
+		return "Ordinador [model = " + model + ", size = " + size + ", cpu = " + cpu + ", ram = " + ram + "]";
+	}
+	
+	public Ordinador(){};
+	
+	public Ordinador(String model, String size, String cpu, String ram) {
+		super();
+		this.model = model;
+		this.size = size;
+		this.cpu = cpu;
+		this.ram = ram;
+	}
+
+	
+
+}
---
+++ clasesGithub/src/vista/Main.java
@@ -0,0 +1,54 @@
+package vista;
+
+import java.util.Scanner;
+
+import controlador.Gestor;
+
+public class Main {
+	
+	Scanner menu = new Scanner(System.in);
+
+	public static void main(String[] args) {
+
+		Gestor gest = new Gestor();
+		Main a = new Main();
+		a.menuNumeric();
+	}
+	
+	
+	public  void menuNumeric(){
+
+            int input = 0;
+            int min = 1;
+            int max = 6;
+
+            while(input < min || input > max)
+            {
+            System.out.print("Choose you option: \n 1 - Añadir. \n 2 - Buscar. "	+ 
+            					"\n 3 - Volcar. \n 4 - Recuperar. \n 5 - Modificar. \n 6 - Salir.");
+            input = menu.nextInt();
+            }
+            
+            switch (input) {
+            case 1: System.out.println("añadir"); //Añadir();
+                break;
+
+            case 2: System.out.println("buscar");//Buscar();
+                break;
+
+            case 3: System.out.println("volcar");//Volcar();
+                break;
+
+            case 4: System.out.println("recuperar");//Recuperar();
+                break;
+                
+            case 5: System.out.println("modificar");//Modificar();
+            	break;
+            	
+            case 6: System.exit(0);
+        }
+
+		menuNumeric();
+	}
+
+}
