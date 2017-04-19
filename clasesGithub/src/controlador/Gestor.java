43a02b393423dce4c83f1d2bc86602631a7ca68b
Mohammed Mizab <mmizab1996@gmail.com>
2017-04-19 19:36:13 +0200

    Add method add() on Gestor class

--- clasesGithub/src/controlador/Gestor.java
+++ clasesGithub/src/controlador/Gestor.java
@@ -1,10 +1,36 @@
 package controlador;
 
-public class Gestor {
+import java.util.Scanner;
 
+public class Gestor {
+	
+	private Scanner scanner = new Scanner(System.in);
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
 	}
-
+	
+	public int add(){
+		String model = null;
+		Integer size = null;
+		String cpu = null;
+		Integer ram = null;
+		try{
+			System.out.println("Model: \n");
+			model = scanner.next();
+			System.out.println("Tamaño: \n");
+			size = scanner.nextInt();
+			System.out.println("Processador: \n");
+			cpu = scanner.next();
+			System.out.println("Ram: \n");
+			ram = scanner.nextInt(); 
+		}catch(Exception e){
+			System.out.println(e);
+			return 1;
+		}
+		
+		
+		return 0;
+	}
 }
