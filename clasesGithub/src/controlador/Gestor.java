3d8506f42c35326abfeb8babb997de54bad40bc6
David Abellan <davidabellan89@gmail.com>
2017-04-19 20:12:27 +0200

    Empiezo la función para poder guardar el TreeMap

--- clasesGithub/src/controlador/Gestor.java
+++ clasesGithub/src/controlador/Gestor.java
@@ -1,6 +1,11 @@
 package controlador;
 
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
 import java.util.Scanner;
+import java.util.TreeMap;
+
+import javax.swing.JOptionPane;
 
 public class Gestor {
 	
@@ -32,5 +37,18 @@ public class Gestor {
 		
 		
 		return 0;
+	}
+	
+	public void guardar(TreeMap tree){
+	//El usuario deberá introducir el nombre del fichero en el que guardaremos, y guardaremos el TreeMap directamente	
+		System.out.println("Please, enter the name of the file to save the TreeMap: \n");
+		String fitxer = scanner.next();
+		
+		try{
+	
+		}
+		catch(Exception e){
+			System.out.println(e.getMessage());
+	}
 	}
 }
