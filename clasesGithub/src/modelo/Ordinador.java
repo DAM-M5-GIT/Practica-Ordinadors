4ff45c729f13657c74eba5ca5a41abe8598cc37e
Alejandro Escribano Villagrasa <escribansen@gmail.com>
2017-04-21 15:59:24 +0200

    Agregados comentarios y declaración de treemap.
    Pequeñas correcciones de indentación.

--- clasesGithub/src/modelo/Ordinador.java
+++ clasesGithub/src/modelo/Ordinador.java
@@ -4,43 +4,63 @@ import java.io.Serializable;
 
 public class Ordinador implements Serializable{
 	
+	//DECLARACIÓN DE ATRIBUTOS DE LA CLASE ORDENADOR
 	private String model="";
 	private String size="";
 	private String cpu="";
 	private String ram="";
 	
 	
-	public String getModel() {
+	// SETTERS , GETTERS  y CONSTRUCTORES 
+	public String getModel() 
+	{
 		return model;
 	}
-	public void setModel(String model) {
+	
+	public void setModel(String model) 
+	{
 		this.model = model;
 	}
-	public String getSize() {
+	
+	public String getSize() 
+	{
 		return size;
 	}
-	public void setSize(String size) {
+	
+	public void setSize(String size) 
+	{
 		this.size = size;
 	}
-	public String getCpu() {
+	
+	public String getCpu() 
+	{
 		return cpu;
 	}
-	public void setCpu(String cpu) {
+	
+	public void setCpu(String cpu) 
+	{
 		this.cpu = cpu;
 	}
-	public String getRam() {
+	
+	public String getRam() 
+	{
 		return ram;
 	}
-	public void setRam(String ram) {
+	
+	public void setRam(String ram) 
+	{
 		this.ram = ram;
 	}
 	
+	//TO STRING PARA MOSTRAR FRASES POR PANTALLA
 	public String toString() {
 		return "Computer [Model = " + model + ", Size = " + size + ", Cpu = " + cpu + ", RAM = " + ram + "]";
 	}
 	
+	//CONSTRUCTOR VACÍO
 	public Ordinador(){};
 	
+	//CONSTRUCTOR CON PARÁMETROS
 	public Ordinador(String model, String size, String cpu, String ram) {
 		super();
 		this.model = model;
--- clasesGithub/src/vista/Main.java
+++ clasesGithub/src/vista/Main.java
@@ -1,28 +1,34 @@
 package vista;
 
 import java.util.Scanner;
+import java.util.TreeMap;
 
 import controlador.Gestor;
 
 public class Main {
 	
-	
-
+	// MENÚ PRINCIPAL
 	public static void main(String[] args) {
 
-            int input = 0;
+            int input = 0; // VARIABLE DE USO PARA SELECCIÓN DE OPCIONES
      
+            //DECLARACiÓN DE SCANNER PARA LECTURA DE OPCIONES TECLEADAS
             Scanner menu = new Scanner(System.in);
         	
+            //DECLARACIÓN DEL GESTOR PARA EL USO DE MÉTODOS DEL MISMO
         	Gestor gest = new Gestor();
+        	
+        	//DECLARACIÓN DEL TREEMAP
+        	TreeMap tm = new TreeMap();
 
             while(input != 6)
             {
+            	//MUESTRA DE OPCIONES DEL MENÚ
             System.out.print("Choose your option: \n 1 - Add. \n 2 - Search. "	+ 
             					"\n 3 - Save . \n 4 - Open file. \n 5 - Modify. \n 6 - Exit.");
             input = menu.nextInt();
             
-            
+            //USO DE SWICH PARA EL DIRECCIONAMIENTO DE LAS OPCIONES SELLECIONADAS
             switch (input) {
             case 1: System.out.println("Add"); //Añadir();
                 break;
